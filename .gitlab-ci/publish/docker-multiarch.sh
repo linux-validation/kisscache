4c2a3a4149c178e0bcd06c3f5dbffa3dcb85903e
Matt Hart <matthart@qti.qualcomm.com>
2026-03-17 11:46:08 +0000

    Add github workflows for build and test
    Signed-off-by: Matt Hart <matthart@qti.qualcomm.com>

--- .gitlab-ci/publish/docker-multiarch.sh
+++ .gitlab-ci/publish/docker-multiarch.sh
@@ -12,8 +12,9 @@ else
 
   # Default values
   CI_REGISTRY_IMAGE=${CI_REGISTRY_IMAGE:-"registry.gitlab.com/linaro/kisscache"}
+  CI_REGISTRY_IMAGE="${CI_REGISTRY_OVERRIDE:-CI_REGISTRY_IMAGE}"
 
-  docker manifest create $CI_REGISTRY_IMAGE:latest $CI_REGISTRY_IMAGE/kisscache-amd64:latest $CI_REGISTRY_IMAGE/kisscache-arm64:latest
+  docker manifest create $CI_REGISTRY_IMAGE:latest $CI_REGISTRY_IMAGE-amd64:latest $CI_REGISTRY_IMAGE-arm64:latest
   # Push only for tags or master
   if [ "$CI_COMMIT_REF_SLUG" = "master" -o -n "$CI_COMMIT_TAG" ]
   then
